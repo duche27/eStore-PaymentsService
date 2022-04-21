4f193e1addfd26cee3ac3d2008536b4112bddc03
Guillermo Fernández <guillermo_fernandez_raboso@carrefour.com>
2022-04-21 10:26:08 +0200

    implementación de ShipmentService sin compensation

--- src/main/java/com/example/paymentsservice/exceptions/PaymentErrorHandler.java
+++ src/main/java/com/example/paymentsservice/exceptions/PaymentErrorHandler.java
@@ -1,12 +1,18 @@
 package com.example.paymentsservice.exceptions;
 
 import org.axonframework.commandhandling.CommandExecutionException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class PaymentErrorHandler {
@@ -19,13 +25,6 @@ public class PaymentErrorHandler {
         return new ResponseEntity<>(errorMessage, HttpStatus.NO_CONTENT);
     }
 
-    @ExceptionHandler(value = {RuntimeException.class})
-    public ResponseEntity<ErrorMessage> handleRuntimeException(RuntimeException e) {
-
-        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
-        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
     // excepciones del AGGREGATE
     @ExceptionHandler(value = {CommandExecutionException.class})
     public ResponseEntity<ErrorMessage> handleCommandExecutionException(CommandExecutionException e, WebRequest request) {
@@ -36,6 +35,35 @@ public class PaymentErrorHandler {
 
     @ExceptionHandler(value = {IllegalStateException.class})
     public ResponseEntity<ErrorMessage> handleIllegalStateException(IllegalStateException e) {
+
+        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // para manejar excepciones de validaciones de constraints de hibernate
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorMessage> dataIntegrityViolationHandler(DataIntegrityViolationException e) {
+
+        ErrorMessage error = new ErrorMessage(e.getMostSpecificCause().getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    // para manejar excepciones de validaciones de hibernate con @Valid
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> fieldsValidationExceptions(MethodArgumentNotValidException e) {
+
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(ErrorMessage.builder().message(errors.toString()).build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {RuntimeException.class})
+    public ResponseEntity<ErrorMessage> handleRuntimeException(RuntimeException e) {
 
         ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
